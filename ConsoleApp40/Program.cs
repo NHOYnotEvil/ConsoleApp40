cfc11f91c55e9c6134818dd5c3f502d2b94a9cd8
NHOY <NHOY@2210DR000919>
2023-05-30 01:13:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp40
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 5;
+            string commandCloseProgram = "exit";
+            string userInput;
+
+            for (int i = 0; i < tryCount; i++)
+            {
+                Console.WriteLine("Команда для зыкрытия программы - \"exit\"");
+                Console.Write("Введите команду для закрытия программы:");
+                userInput = Console.ReadLine();
+
+                if (commandCloseProgram == userInput)
+                {
+                    break;
+                }
+                else
+                {
+                    tryCount++;
+                    Console.WriteLine("Попробуйте снова");
+                }
+            }
+        }
+    }
+}
